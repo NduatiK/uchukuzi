d26085b6b93352cd6f1ed3f9fa2a7443a59f6918
NduatiK <012nkuria@gmail.com>
2020-03-16 21:15:18 +0300

    Add bus

---
+++ lib/uchukuzi/school/bus.ex
@@ -0,0 +1,51 @@
+defmodule Uchukuzi.School.Bus do
+  alias __MODULE__
+
+  alias Uchukuzi.School.Device
+  alias Uchukuzi.School.Route
+  alias Uchukuzi.Roles.Assistant
+
+  @enforce_keys [:number_plate, :device, :route, :assistants]
+  defstruct [:number_plate, :device, :route, :assistants]
+
+  def new(number_plate, %Device{} = device, %Route{} = route) do
+    with :ok <- validate_number_plate(number_plate) do
+      {:ok, %Bus{number_plate: number_plate, device: device, route: route, assistants: []}}
+    else
+      {:error, msg} ->
+        {:error, msg}
+    end
+  end
+
+  defp validate_number_plate(number_plate) when is_binary(number_plate) do
+    invalid_characters = ["I", "O"]
+
+    contains_invalid_characters =
+      invalid_characters
+      |> Enum.any?(&String.contains?(number_plate, &1))
+
+    if contains_invalid_characters do
+      {:error, "should not contain any letter in #{invalid_characters}"}
+    end
+
+    {:ok, reg} = Regex.compile("^K[A-Z]{2}\\d{3}[A-Z]{0,1}$")
+
+    if Regex.match?(reg, number_plate) do
+      :ok
+    else
+      {:error, "The number plate \"#{number_plate}\" has an invalid format"}
+    end
+  end
+
+  defp validate_number_plate(garbage) do
+    {:error, "A number plate should be a string, you gave a #{garbage}"}
+  end
+
+  def assign_assistant(%Bus{} = bus, %Assistant{} = assistant) do
+    %Bus{bus | assistants: [assistant | bus.assistants]}
+  end
+
+  def remove_assistant(%Bus{} = bus, %Assistant{} = assistant) do
+    %Bus{bus | assistants: Enum.filter(bus.assistants, &(&1 != assistant))}
+  end
+end
