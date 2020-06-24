1835b4490f3680cb66028be667c54567d9e161b4
NduatiK <012nkuria@gmail.com>
2020-06-24 07:24:19 +0300

    Refactor Report

--- apps/uchukuzi/lib/uchukuzi/common/report.ex
+++ apps/uchukuzi/lib/uchukuzi/common/report.ex
@@ -16,11 +16,6 @@ defmodule Uchukuzi.Common.Report do
     %Report{time: time, location: location}
   end
 
-  def is_report(%__MODULE__{}), do: true
-  def is_report(_), do: false
-
-  def to_report(%{location: %Location{} = location, time: time}), do: new(time, location)
-
   def to_coord(%{location: %Location{} = location}),
     do: Location.to_coord(location)
 end
