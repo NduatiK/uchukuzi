f2786d7fa6fa809e1cb2c9f67af2eab7ebaf460f
NduatiK <012nkuria@gmail.com>
2020-07-14 18:33:02 +0300

    Remove force ssl

--- apps/uchukuzi_interface/config/prod.exs
+++ apps/uchukuzi_interface/config/prod.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :uchukuzi_interface, UchukuziInterfaceWeb.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: "uchukuzi.gigalixirapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  # force_ssl: [rewrite_on: [:x_forwarded_proto]],  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
   server: true,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
